2472ec84eddd68ce2cd11a7a9ad27a2449c40b31
Jesse Squires <jesse.d.squires@gmail.com>
2015-02-07 17:27:29 -0800

    fix cell factory

--- JSQDataSourcesKit/JSQDataSourcesKit/TableViewDataSource.swift
+++ JSQDataSourcesKit/JSQDataSourcesKit/TableViewDataSource.swift
@@ -67,7 +67,7 @@ public struct TableViewCellFactory<Cell: UITableViewCell, DataItem>: TableViewCe
 
     public func cellForItem(item: DataItem, inTableView tableView: UITableView, atIndexPath indexPath: NSIndexPath) -> Cell {
         let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath) as Cell
-        return cell
+        return configureCell(cell, forItem: item, inTableView: tableView, atIndexPath: indexPath)
     }
 
     public func configureCell(cell: Cell, forItem item: DataItem, inTableView tableView: UITableView, atIndexPath indexPath: NSIndexPath) -> Cell {
