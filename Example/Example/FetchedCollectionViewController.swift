3f7df03a64976c222216e8b47c572cd24f7a4ade
Jesse Squires <jesse.d.squires@gmail.com>
2015-02-21 17:28:39 -0800

    finish example for CollectionViewFetchedResultsDataSourceProvider

--- Example/Example/FetchedCollectionViewController.swift
+++ Example/Example/FetchedCollectionViewController.swift
@@ -23,9 +23,39 @@ import JSQDataSourcesKit
 class FetchedCollectionViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
-    
+
+    let stack = CoreDataStack()
+
+    var dataSourceProvider: CollectionViewFetchedResultsDataSourceProvider<Thing, CollectionViewCellFactory<CollectionViewCell, Thing> >?
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
+
+        collectionView.registerNib(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: collectionCellId)
+
+        let factory = CollectionViewCellFactory(reuseIdentifier: collectionCellId) { (cell: CollectionViewCell, model: Thing, collectionView: UICollectionView, indexPath: NSIndexPath) -> CollectionViewCell in
+            cell.label.text = model.displayName
+            cell.label.textColor = UIColor.whiteColor()
+            cell.backgroundColor = model.displayColor
+            return cell
+        }
+
+        let frc: NSFetchedResultsController = NSFetchedResultsController(fetchRequest: Thing.fetchRequest(), managedObjectContext: stack.context, sectionNameKeyPath: "category", cacheName: nil)
+
+        self.dataSourceProvider = CollectionViewFetchedResultsDataSourceProvider(fetchedResultsController: frc, cellFactory: factory, collectionView: self.collectionView)
+    }
+
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        self.dataSourceProvider?.fetchedResultsController.performFetch(nil)
     }
 
 }
