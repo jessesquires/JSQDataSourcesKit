2d3f275d09a34d80556c0d6358ad4c97341ea3e5
Jesse Squires <jesse.d.squires@gmail.com>
2015-02-28 23:58:25 -0800

    begin CollectionViewDataSource tests

---
+++ JSQDataSourcesKit/JSQDataSourcesKitTests/CollectionViewDataSourceTests.swift
@@ -0,0 +1,42 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQDataSourcesKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQDataSourcesKit
+//
+//
+//  License
+//  Copyright (c) 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import UIKit
+import Foundation
+import XCTest
+import JSQDataSourcesKit
+
+
+// MARK: test case
+
+class CollectionViewDataSourceTests: XCTestCase {
+
+    // MARK: setup
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+
+    // MARK: tests
+
+}
