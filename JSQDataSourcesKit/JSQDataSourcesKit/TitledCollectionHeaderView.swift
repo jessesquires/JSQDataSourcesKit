4d1c246be0ab7d600fc5e781b2a2ebab10518591
Jesse Squires <jesse.d.squires@gmail.com>
2015-03-07 16:04:28 -0800

    add TitledCollectionHeaderView #3

---
+++ JSQDataSourcesKit/JSQDataSourcesKit/TitledCollectionHeaderView.swift
@@ -0,0 +1,87 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQDataSourcesKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQDataSourcesKit
+//
+//
+//  License
+//  Copyright (c) 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import UIKit
+
+public class TitledCollectionHeaderView: UICollectionReusableView {
+
+    public typealias ConfigurationHandler = (TitledCollectionHeaderView) -> Void
+
+    @IBOutlet private weak var _label: UILabel!
+    @IBOutlet private weak var _leftSpacing: NSLayoutConstraint!
+    @IBOutlet private weak var _topSpacing: NSLayoutConstraint!
+    @IBOutlet private weak var _rightSpacing: NSLayoutConstraint!
+    @IBOutlet private weak var _bottomSpacing: NSLayoutConstraint!
+
+    public var label: UILabel {
+        get {
+            return _label
+        }
+    }
+
+    public var leftSpacing: NSLayoutConstraint {
+        get {
+            return _leftSpacing
+        }
+    }
+
+    public var topSpacing: NSLayoutConstraint {
+        get {
+            return _topSpacing
+        }
+    }
+
+    public var rightSpacing: NSLayoutConstraint {
+        get {
+            return _rightSpacing
+        }
+    }
+
+    public var bottomSpacing: NSLayoutConstraint {
+        get {
+            return _bottomSpacing
+        }
+    }
+
+    public override var backgroundColor: UIColor? {
+        get {
+            return super.backgroundColor
+        }
+        set {
+            super.backgroundColor = newValue
+            label.backgroundColor = newValue
+        }
+    }
+
+    public class var identifier: String {
+        get {
+            return toString(TitledCollectionHeaderView.self)
+        }
+    }
+
+    public class var nib: UINib {
+        get {
+            return UINib(nibName: "TitledCollectionHeaderView", bundle: NSBundle(forClass: TitledCollectionHeaderView.self))
+        }
+    }
+
+    public func configureWithHandler(handler: ConfigurationHandler) {
+        handler(self)
+    }
+    
+}
