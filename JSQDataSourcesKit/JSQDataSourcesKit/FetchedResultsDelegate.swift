5b364a0fac7b2bcea0fe12d0e76355f490c58109
Jesse Squires <jesse.d.squires@gmail.com>
2015-03-14 18:55:40 -0700

    finish docs for fetched results delegates. #1

--- JSQDataSourcesKit/JSQDataSourcesKit/FetchedResultsDelegate.swift
+++ JSQDataSourcesKit/JSQDataSourcesKit/FetchedResultsDelegate.swift
@@ -21,7 +21,17 @@ import UIKit
 import CoreData
 
 
-// here, `DataItem` is Phantom Type
+///  A `CollectionViewFetchedResultsDelegateProvider` is responsible for providing a delegate object
+///  for an instance of `NSFetchedResultsController` that manages data to display in a collection view.
+///  <br/><br/>
+///  **The delegate provider has the following type parameters:**
+///  <br/>
+///  ````
+///  <DataItem>
+///  ````
+///  <br/>
+///  Here, the `DataItem` type parameter acts as a [phatom type](http://www.objc.io/snippets/13.html). 
+///  This type should correpsond to the type of objects that the `NSFetchedResultsController` fetches.
 public final class CollectionViewFetchedResultsDelegateProvider <DataItem> {
 
     private typealias SectionIndex = Int
@@ -30,10 +40,18 @@ public final class CollectionViewFetchedResultsDelegateProvider <DataItem> {
     private typealias ObjectIndexPaths = [NSIndexPath]
     private typealias ObjectChangesDictionary = [NSFetchedResultsChangeType : ObjectIndexPaths]
 
+    ///  The collection view that displays the data from the `NSFetchedResultsController` for which this provider provides a delegate.
     public weak var collectionView: UICollectionView?
 
+    ///  Returns the object that is notified when the fetched results changed.
     public var delegate: NSFetchedResultsControllerDelegate { return bridgedDelegate }
 
+    ///  Constructs a new delegate provider for a fetched results controller.
+    ///
+    ///  :param: collectionView The collection view to be updated when the fetched results change.
+    ///  :param: controller     The fetched results controller whose delegate will be provided by this provider.
+    ///
+    ///  :returns: A new `CollectionViewFetchedResultsDelegateProvider` instance.
     public init(collectionView: UICollectionView, controller: NSFetchedResultsController? = nil) {
         self.collectionView = collectionView
 
@@ -132,16 +150,34 @@ public final class CollectionViewFetchedResultsDelegateProvider <DataItem> {
 
 }
 
-
+///  A `TableViewFetchedResultsDelegateProvider` is responsible for providing a delegate object
+///  for an instance of `NSFetchedResultsController` that manages data to display in a table view.
+///  <br/><br/>
+///  **The delegate provider has the following type parameters:**
+///  <br/>
+///  ````
+///  DataItem, CellFactory: TableViewCellFactoryType
+///  where CellFactory.DataItem == DataItem>
+///  ````
 public final class TableViewFetchedResultsDelegateProvider <DataItem, CellFactory: TableViewCellFactoryType
                                                             where CellFactory.DataItem == DataItem> {
 
+    ///  The table view that displays the data from the `NSFetchedResultsController` for which this provider provides a delegate.
     public weak var tableView: UITableView?
 
+    ///  Returns the cell factory for this delegate provider.
     public let cellFactory: CellFactory
 
+    ///  Returns the object that is notified when the fetched results changed.
     public var delegate: NSFetchedResultsControllerDelegate { return bridgedDelegate }
 
+    ///  Constructs a new delegate provider for a fetched results controller.
+    ///
+    ///  :param: tableView   The table view to be updated when the fetched results change.
+    ///  :param: cellFactory The cell factory from which the fetched results controller delegate will configure cells.
+    ///  :param: controller  The fetched results controller whose delegate will be provided by this provider.
+    ///
+    ///  :returns: A new `TableViewFetchedResultsDelegateProvider` instance.
     public init(tableView: UITableView, cellFactory: CellFactory, controller: NSFetchedResultsController? = nil) {
         self.tableView = tableView
         self.cellFactory = cellFactory
