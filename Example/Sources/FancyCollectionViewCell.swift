3ed027c7ca6b747e2671e55d5a03a13387dcd46b
Dave Caunt <dcaunt@gmail.com>
2016-07-25 16:34:42 +0100

    Add example with mixed cell types

---
+++ Example/Sources/FancyCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://jessesquires.com/JSQDataSourcesKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQDataSourcesKit
+//
+//
+//  License
+//  Copyright © 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import UIKit
+
+class FancyCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+
+    var testing = false
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectedBackgroundView = UIView()
+        selectedBackgroundView?.backgroundColor = .orangeColor()
+        backgroundColor = .greenColor()
+    }
+}
