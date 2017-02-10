5f7a372c8ac34035a90c80b9835b0d5b0e8ff97c
Panagiotis Sartzetakis <panos@sartzetakis.me>
2017-02-10 10:47:02 +0000

    passing the item back to the closure

--- Source/TableEditingController.swift
+++ Source/TableEditingController.swift
@@ -22,28 +22,29 @@ import UIKit
 /**
  An instance of `TableEditingController` allows editing a table view via inserting and deleting rows.
  */
-public struct TableEditingController {
+public struct TableEditingController<Item> {
 
     // MARK: Typealiases
 
     /**
      Asks if a row at the specified index path is editable for the specified table view.
-
+     - parameter item:      The item at `indexPath`.
      - parameter tableView: The table view requesting this information.
      - parameter indexPath: The index path of the item.
 
      - returns: `true` if the specified row is editable, `false` otherwise.
      */
-    public typealias CanEditRowConfig = (_ tableView: UITableView, _ indexPath: IndexPath) -> Bool
+    public typealias CanEditRowConfig = (_ item: Item?, _ tableView: UITableView, _ indexPath: IndexPath) -> Bool
 
     /**
      Commits the editing actions for the specified index path.
 
+     - parameter item:      The item at `indexPath`.
      - parameter tableView: The table view being edited.
      - parameter commit:    The editing style.
      - parameter indexPath: The index path of the item.
      */
-    public typealias CommitEditingConfig = (_ tableView: UITableView, _ commit: UITableViewCellEditingStyle, _ indexPath: IndexPath) -> Void
+    public typealias CommitEditingConfig = ( _ item: Item?, _ tableView: UITableView, _ commit: UITableViewCellEditingStyle, _ indexPath: IndexPath) -> Void
 
     /// A closure that determines if a given row is editable.
     public let canEditRow: CanEditRowConfig
