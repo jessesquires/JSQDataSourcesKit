32885fd3a91c77236372892627bf0d7503b591a3
Jesse Squires <jessesquires@users.noreply.github.com>
2018-01-22 14:54:02 -0800

    Update build_docs.sh

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -4,13 +4,21 @@
 # https://github.com/realm/jazzy
 # ------------------------------
 
-jazzy \
-    --clean \
-    --author 'Jesse Squires' \
-    --author_url 'https://twitter.com/jesse_squires' \
-    --github_url 'https://github.com/jessesquires/JSQDataSourcesKit' \
-    --module 'JSQDataSourcesKit' \
-    --source-directory . \
-    --readme 'README.md' \
-    --documentation 'Guides/*.md' \
-    --output docs/ \
+if which jazzy >/dev/null; then
+    jazzy \
+        --clean \
+        --author 'Jesse Squires' \
+        --author_url 'https://twitter.com/jesse_squires' \
+        --github_url 'https://github.com/jessesquires/JSQDataSourcesKit' \
+        --module 'JSQDataSourcesKit' \
+        --source-directory . \
+        --readme 'README.md' \
+        --documentation 'Guides/*.md' \
+        --output docs/ \;
+else
+    echo "
+    Error: jazzy not installed! <https://github.com/realm/jazzy>
+    Install: gem install jazzy
+    "
+    exit 1
+fi
