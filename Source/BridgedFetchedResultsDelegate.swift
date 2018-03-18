845e3bfcab22126887fe2db9bef31f8f68689ed0
Jesse Squires <jessesquires@users.noreply.github.com>
2018-03-18 16:24:07 -0700

    Update BridgedFetchedResultsDelegate.swift

--- Source/BridgedFetchedResultsDelegate.swift
+++ Source/BridgedFetchedResultsDelegate.swift
@@ -21,7 +21,6 @@ import Foundation
 
 /// This class is responsible for implementing the `NSFetchedResultsControllerDelegate` protocol.
 /// It avoids making `FetchedResultsDelegateProvider` inherit from `NSObject`, and keeps classes small and focused.
-///
 @objc
 internal final class BridgedFetchedResultsDelegate: NSObject {
 
