befff242e30999b3ee49ca31c35855215eb32bec
Jesse Squires <jesse@jessesquires.com>
2019-03-08 18:23:50 -0800

    Minor clean up and formatting

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
 source "https://rubygems.org"
 
 gem 'cocoapods', '~> 1.6.0'
+
 gem 'danger', '~> 5.0'
 gem 'danger-swiftlint'
 gem 'danger-xcode_summary'
-gem 'xcpretty', '~> 0.3.0'
-gem 'xcpretty-json-formatter'
 gem 'danger-auto_label'
 gem 'danger-prose'
+
+gem 'xcpretty', '~> 0.3.0'
+gem 'xcpretty-json-formatter'
--- scripts/test_danger.sh
+++ scripts/test_danger.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-function run_against_pr {
-    cd ..; bundle install
-    
-    # Use the provided PR to run danger against.
-    bundle exec danger pr https://github.com/jessesquires/JSQDataSourcesKit/pull/$1
-}
-
-######################
-# Run danger locally #
-######################
-
+# Test running danger locally
 if [ $# = 1 ] ; then
-    run_against_pr $1
+    bundle exec danger pr https://github.com/jessesquires/JSQDataSourcesKit/pull/$1
 else
-    echo "No arguments given... You MUST provide a valid github PR number."
+    echo "No arguments given. Provide a valid GitHub PR number."
     echo "Example usage: sh $0 120"
     exit 0
 fi
